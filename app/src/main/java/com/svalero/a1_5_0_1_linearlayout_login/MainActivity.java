d39c3da1e73999e32292b8d1f016ed3557fb394c
Alberto Herandez <albertohdez@gmail.com>
2020-09-17 13:42:26 +0200

    Initial commit

---
+++ app/src/main/java/com/svalero/a1_5_0_1_linearlayout_login/MainActivity.java
@@ -0,0 +1,13 @@
+package com.svalero.a1_5_0_1_linearlayout_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
