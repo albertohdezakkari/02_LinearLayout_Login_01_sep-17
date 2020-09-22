8739f5116b4e1d44ed56befde4be072493fff2c3
Alberto Herandez <albertohdez@gmail.com>
2020-09-22 13:15:53 +0200

    1º

--- app/src/main/java/com/svalero/a1_5_0_1_linearlayout_login/MainActivity.java
+++ app/src/main/java/com/svalero/a1_5_0_1_linearlayout_login/MainActivity.java
@@ -3,11 +3,39 @@ package com.svalero.a1_5_0_1_linearlayout_login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.svalero.a1_5_0_1_linearlayout_login.interfaces.MyOnClickListener;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnLogin;
+    private EditText edtEmail;
+    private EditText edtPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initComponents();
+        // edtEmail.getText().toString();
+        // edtPassword.getText().toString();
+        // métodos flecha
+        // MainActivity = 0X154879F
+        btnLogin.setOnClickListener(new MyOnClickListener(this));
+    }
+    private void initComponents(){
+        btnLogin = findViewById(R.id.btnLogin);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword =findViewById(R.id.edtPassword);
+    }
+    public EditText getEdtEmail() {
+        return edtEmail;
+    }
+    public EditText getEdtPassword() {
+        return edtPassword;
     }
 }
---
+++ app/src/main/java/com/svalero/a1_5_0_1_linearlayout_login/interfaces/MyOnClickListener.java
@@ -0,0 +1,26 @@
+package com.svalero.a1_5_0_1_linearlayout_login.interfaces;
+
+import android.view.View;
+import android.widget.Toast;
+
+import com.svalero.a1_5_0_1_linearlayout_login.MainActivity;
+
+public class MyOnClickListener
+        implements View.OnClickListener {
+    private MainActivity padre; //0X154879F
+    // 0X154879F
+    public MyOnClickListener(MainActivity padre) {
+        this.padre = padre;
+        // 0X154879F
+    }
+    @Override
+    public void onClick(View view) {
+                String resp = "Email: " + padre.getEdtEmail().
+                        getText().toString();
+                resp+="- Password: " + padre.getEdtPassword().
+                        getText().toString();
+                Toast.makeText(padre.getBaseContext(),
+                        resp,
+                        Toast.LENGTH_SHORT).show();
+            }
+}
